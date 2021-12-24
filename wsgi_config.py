5aaebb01c7027eb0dfde774289e8e9a3aa1df9b2
AJ Auntor <auntorknocks@gmail.com>
2021-12-24 21:47:40 +0600

    wsgi none

--- wsgi_config.py
+++ wsgi_config.py
@@ -1,5 +1,4 @@
-# -*- coding:utf-8 -*-
-# wossl.service加载配置文件
+
 import os
 import gevent.monkey
 gevent.monkey.patch_all()
@@ -11,6 +10,5 @@ loglevel='debug'
 bind='192.168.85.81:8080'
 pidfile='/tmp/gunicorn.pid'
 logfile='/tmp/debug.log'
-# 多核多线程
 workers = multiprocessing.cpu_count()*2 + 1
 worker_class = 'gunicorn.workers.ggevent.GeventWorker'
